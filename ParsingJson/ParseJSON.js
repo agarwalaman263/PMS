9aa675e66e0cc283f9d9eb4e9d290a2e2e3518dc
Aman Agarwal <cyclopaman@gmail.com>
2019-07-17 23:01:56 +0530

    eslint enabled

--- ParsingJson/ParseJSON.js
+++ ParsingJson/ParseJSON.js
@@ -1,13 +1,12 @@
-let ParseJSON=(data)=>{
-	return new Promise((resolve,reject)=>{
-		try{
-			resolve(JSON.parse(data))
-		}
-		catch(e){
-			reject('Unable to parse the JSON')
-		}
-	})
+let ParseJSON = (data) => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(JSON.parse(data))
+        } catch (e) {
+            reject('Unable to parse the JSON')
+        }
+    })
 }
-module.exports={
-	"main": ParseJSON
+module.exports = {
+    "main": ParseJSON
 }
--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const fs = require('fs')
 const ReadJson = require('./ReadingJson/ReadingJson')
 const ParsingJSON = require('./ParsingJSON/ParseJSON')
-
-if (process.argv.length != 3) {
+const ProcessArray = process.argv
+if (ProcessArray != 3) {
     console.log("The parameter is incorrectly passed. Kindly Check into it. ")
 } else {
-    let PathToData = process.argv[2];
+    let PathToData = ProcessArray[2];
     let DataStored = null;
     let FeatureStored = null;
     ReadJson.main(PathToData)
         .then(data => ParsingJSON.main(data))
         .then(data => {
-        	DataStored=data;
-        	return ReadJson.main("./Database/feature.json")
+            DataStored = data;
+            return ReadJson.main("./Database/feature.json")
         })
-        .then(data=>ParsingJSON.main(data))
-        .then(data=>{FeatureStored=data})
+        .then(data => ParsingJSON.main(data))
+        .then(data => { FeatureStored = data })
 
-        .catch(error => console.log(error))
+    .catch(error => console.log(error))
 
 }
