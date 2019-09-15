7332ab36560d7a3e883294e993de83bee6b2fbe7
Agarwal <Aman.Agarwal@philips.com>
2019-09-15 23:32:44 +0530

    reverted

---
+++ JavaScript Business Logic/index.js
@@ -0,0 +1,90 @@
+var server = require('ws').Server
+const fs = require('fs')
+var client = require('ws')
+const generatorValidator = require('./generatorValidator/index')
+const LogPrinter = require('./LogPrinter/index')
+let ListOffeatures = [];
+let dataReceiver = new client('ws://localhost:8098/generator');
+var serverInstance = new server({
+    port: 5001
+}, () => {
+    fs.readFile('./features/Feature.json', 'utf8', (error, data) => {
+        ListOffeatures = JSON.parse(data)
+        console.log(ListOffeatures)
+    })
+})
+// dataReceiver.on('open',()=>{
+//     dataReceiver.send('aman')
+// })
+let SetOfPatients = new Set([]);
+serverInstance.on('connection', (ws) => {
+    dataReceiver.send('aman')
+    dataReceiver.on('message', (message) => {
+        // console.log(message)
+        message = JSON.parse(message)
+        console.log(message.patientDemographics.patientId)
+        if (message["patientDemographics"]['type'] == 'GeneratorSetter') {
+            generatorValidator.main(ws._socket.remoteAddress).then(() => {
+                SetOfPatients.add(message['patientDemographics']['patientId'])
+            })
+                .catch(err => {
+                    ws.send('Not A valid generator')
+                    console.log('not a valid generator', err)
+                    ws.close();
+                })
+        }
+        else if (message["patientDemographics"]['type'] == 'Generator') {
+            generatorValidator.main(ws._socket.remoteAddress).then(() => {
+                serverInstance.clients.forEach((client) => {
+                    if (client != ws && client.type == 'Subscriber' && client.patientId == message["patientDemographics"].patientId) {
+                        client.send(JSON.stringify(LogPrinter.main(message["patientVitals"], ListOffeatures)))
+                    }
+                })
+            })
+                .catch(err => {
+                    ws.send('Error Occured')
+                    console.log('Error occured', err)
+                    ws.close();
+                })
+        }
+
+        else {
+            ws.send('Invalid Type1');
+            ws.close();
+        }
+    })
+
+    ws.on('message', (message) => {
+        message = JSON.parse(message)
+        console.log(message.type)
+        if (message["type"] == 'SubscriberSetter') {
+            console.log('aman')
+            if (SetOfPatients.has(message['patientId'])) {
+                console.log('setted')
+                ws['type'] = 'Subscriber'
+                ws['patientId'] = message['patientId']
+            }
+            else {
+                ws.emit("The patient monitorring is switched off. Contact the PMS manager")
+                ws.close();
+            }
+        }
+        else if (message.type == 'Subscriber') {
+            ws.send('Subscriber is not allowed to send Data');
+            ws.close();
+            console.log('Subscriber is not allowed to send Data');
+        }
+        else {
+            ws.send('Invalid Type');
+            ws.close();
+        }
+    }
+    )
+    ws.on('error', (error) => {
+        console.log(JSON.stringify(error))
+        ws.close();
+    })
+    ws.on('close', () => {
+        console.log('Closed')
+    })
+})
---
+++ java logic/pms/src/main/java/com/philips/pms/objects/Constants.java
@@ -0,0 +1,32 @@
+package com.philips.pms.objects;
+
+public class Constants {
+	public static final int PULSE_RATE_MAX=120;
+	public static final int PULSE_RATE_MIN=40;
+
+	public static final int TEMPERATURE_MAX=100;
+	public static final int TEMPERATURE_MIN=196;
+
+	public static final int  INSULINE_LEVEL_MAX=140;
+	public static final int  INSULINE_LEVEL_MIN=55;
+	
+	public static final int  PLATELET_COUNT_MAX=480000;
+	public static final int  PLATELET_COUNT_MIN=130000;
+	
+	public static final int  RBC_COUNT_MAX=6500000;
+	public static final int  RBC_COUNT_MIN=4000000;
+
+
+	public static final int  THYROID_LEVEL_MAX=2;
+	public static final int  THYROID_LEVEL_MIN=0;
+	
+	public static final String DECIMAL_FORMATTER="#.##";
+	
+	public static final String FILE_FORMAT=".json";
+	public static final String FILE_LOCATION=System.getProperty("user.dir")+"\\Data\\data-";
+	public static final String LOGS_FILE_NAME="\\log.txt";
+	public static final String LOGS_FILE_LOCATION=System.getProperty("user.dir");
+	
+
+
+}
